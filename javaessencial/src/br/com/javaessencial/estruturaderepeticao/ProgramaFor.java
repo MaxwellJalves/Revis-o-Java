0571879c0e54f06afd10684337d2ad312491428b
Maxwell.Costa <maxwell880@gmail.com>
2020-11-21 00:07:13 -0300

    Realizado ajuste na exibicao dos dados no console

--- javaessencial/src/br/com/javaessencial/estruturaderepeticao/ProgramaFor.java
+++ javaessencial/src/br/com/javaessencial/estruturaderepeticao/ProgramaFor.java
@@ -1,52 +1,54 @@
 package br.com.javaessencial.estruturaderepeticao;
+
 import java.util.Scanner;
+
 public class ProgramaFor {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+
 		String lista[] = new String[5];
 		String nome;
 		try {
-			for(int i=0; i< lista.length; i++) {
+			for (int i = 0; i < lista.length; i++) {
 				System.out.println(" \n Informe seu nome para que possamos cadastrar no sistema.!");
 				nome = in.nextLine();
-				if(nome.isEmpty()) {
+				if (nome.isEmpty()) {
 					System.out.println("Valor invalido , proxima tentativa");
 					break;
-				}else {
+				} else {
 					lista[i] = nome;
 				}
-				System.out.println(" Registro Salvo com sucesso: Código '%d' "+lista[i]);
+				exibir(lista[i]);
+
 			}
-			
-			for(int i=0; i < lista.length; i++) {
-				exibir();
+
+			for (int i = 0; i < lista.length; i++) {
+
 				System.out.println(lista[i]);
-				String n  = (lista[i].contains("max") ) ? localizarRegistro(i,lista[i]) : "não localizado"  ;
-				if(n.equals("max")) {
+				String n = (lista[i].contains("max")) ? localizarRegistro(i, lista[i]) : "não localizado";
+				if (n.equals("max")) {
 					System.out.println(n);
 				}
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			in.close();	
+		} finally {
+			in.close();
 		}
-		
-		
-	 
+
 	}
-  protected static void exibir() {
-	  StringBuffer sb = new StringBuffer();
-	  sb.append(" ##-- [Registros Adicionados] --##");
-	  System.out.println(sb);
-  }
-  
-  protected static String localizarRegistro(int i ,String n) {
-	  StringBuffer sb = new StringBuffer();
-	  sb.append(" ##-- [Registros registro localizado na posição "+i+" |"+ n.toUpperCase()+ " ] --##");
-	  System.out.println(sb);
-	  return "";
-  }
+
+	private static void exibir(String arr) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(" ##-- [Registros Adicionados] --## \n " + arr + " :");
+		System.out.println(sb);
+	}
+
+	private static String localizarRegistro(int i, String n) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(" ##-- [Registros registro localizado na posição " + i + " |" + n.toUpperCase() + " ] --##");
+		System.out.println(sb);
+		return "";
+	}
 }
