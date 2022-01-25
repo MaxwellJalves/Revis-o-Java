6885de697fe6fa9d9ecce11208cbfb7a43ded7fd
MaxwellJalves <maxwell880@gmail.com>
2022-01-25 15:33:44 -0300

    primeiro exemplo capturando o valor de args passa pela configurações do intelijei e imprimindo os dados na tela

---
+++ javautil/br.com.estudojava/src/Object01.java
@@ -0,0 +1,8 @@
+public class Object01 {
+
+    public static void main(String [] args){
+        for(String argumentos : args){
+            System.out.println(argumentos);
+        }
+    }
+            }
